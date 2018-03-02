35cc6f1d7b3bbe5131b7b6012b1e218c2b3a84bf
Abcdefghijklmnopqrstuvwxyzxyz <huang_yue_zhi@outlook.com>
2018-03-02 21:54:06 +0800

    Update UVa1598.cpp
    Update to newest version.

--- UVa1598.cpp
+++ UVa1598.cpp
@@ -1,186 +1,153 @@
-
 #include <iostream>
 #include <cstdio>
 #include <queue>
 #include <map>
-#include <string>
 #include <algorithm>
 using namespace std;
 
 typedef unsigned long long trade_t;
 
-struct Cmd
+struct Command
 {
-	trade_t s,p;
-	trade_t id;
+    trade_t id, p, s;
+    bool type; //True for BUY, false for SELL.
+    bool operator<(const Command &b) const
+    {
+        if(type)
+        {
+            //BUY
+            if(p == b.p) return id < b.id;
+            else return p < b.p;
+        }
+        else
+        {
+            //SELL
+            if(p == b.p) return id < b.id;
+            else return p > b.p;
+        }
+    }
 };
 
-struct BuyCmp
+priority_queue<Command> qbuy, qsell;
+map<trade_t, bool> cancelled;
+map<trade_t, trade_t> buys, sells;
+
+trade_t bid_p, bid_s, ask_p, ask_s;
+
+
+void clearQueue()
 {
-	bool operator()(const Cmd& a, const Cmd& b)
-	{
-		if(a.p < b.p) return true;
-		if(a.p == b.p) return a.id < b.id;
-		if(a.p > b.p) return false;
-	}
-};
+    while(true)
+    {
+    	if(qbuy.empty()) break;
+        trade_t id = qbuy.top().id;
+        if(qbuy.empty() || ((cancelled.count(id) == 0) && (qbuy.top().s > 0))) break;
+        buys[qbuy.top().p] -= qbuy.top().s;
+        qbuy.pop();
+    }
+    while(true)
+    {
+    	if(qsell.empty()) break;
+        trade_t id = qsell.top().id;
+        if(qsell.empty() || (cancelled.count(id) == 0) && (qsell.top().s > 0)) break;
+        sells[qsell.top().p] -= qsell.top().s;
+        qsell.pop();
+    }
+}
 
-struct SellCmp
+void update_quote()
 {
-	bool operator()(const Cmd& a, const Cmd& b)
-	{
-		if(a.p > b.p) return true;
-		if(a.p == b.p) return a.id < b.id;
-		if(a.p < b.p) return false;
-	}
-};
-
-trade_t ask_p,ask_s,bid_p,bid_s;
-map<trade_t, bool> d;
-priority_queue<Cmd, deque<Cmd>, BuyCmp> qbuy;
-priority_queue<Cmd, deque<Cmd>, SellCmp> qsell;
-
-void clear_queue()
-{
-	while(true)
-	{
-
-		Cmd c = qbuy.top();
-		if(!(d.count(c.id) && c.s == 0)) break;
-		qbuy.pop();
-		bid_s -= c.s;
-	}
-	while(true)
-	{
-		Cmd c = qsell.top();
-		if(!(d.count(c.id) && c.s == 0)) break;
-		qsell.pop();
-		ask_s -= c.s;
-	}
+	if (qbuy.empty())
+		bid_p = 0;
+	else
+		bid_p = qbuy.top().p;
+	if (qsell.empty())
+		ask_p = 99999;
+	else
+		ask_p = qsell.top().p;
 }
 
 void print_quote()
 {
-	printf("QUOTE %u %u - %u %u", bid_s, bid_p, ask_s, ask_p);
+	trade_t bid_s = buys[bid_p], ask_s = sells[ask_p];
+	printf("QUOTE %u %u - %u %u\n", bid_s, bid_p, ask_s, ask_p);
 }
 
-void print_trade(trade_t s, trade_t p)
+void trade(trade_t s, trade_t p, trade_t id)
 {
-	printf("TRADE %u %u", s, p);
+	printf("TRADE %u %u\n", s, p);
 }
 
-void clear()
+int _main(unsigned N)
 {
-	while(!qbuy.empty())qbuy.pop();
-	while(!qsell.empty())qsell.pop();
+    for(unsigned i = 1 ; i <= N ; i++)
+    {
+    	update_quote();
+        string cmd;
+        cin >> cmd;
+        if(cmd[0] == 'C')
+        {
+            trade_t id;
+            cin >> id;
+            cancelled[id] = true;
+        }
+        if(cmd[0] == 'B')
+        {
+            Command c;
+            c.type = true;
+            c.id = i;
+            cin >> c.s >> c.p;
+            buys[c.p] += c.s;
+            while(c.p >= ask_p && c.s > 0)
+            {
+                Command c2 = qsell.top();
+                qsell.pop();
+                trade_t amount = min(c2.s, c.s);
+                trade(amount, ask_p, c.id);
+                c2.s -= amount;
+                c.s -= amount;
+                sells[ask_p] -= amount;
+                buys[c.p] -= amount;
+                if(c2.s > 0) qsell.push(c2);
+                update_quote();
+            }
+            qbuy.push(c);
+        }
+        if(cmd[0] == 'S')
+        {
+            Command c;
+            c.type = false;
+            c.id = i;
+            cin >> c.s >> c.p;
+            sells[c.p] += c.s;
+            while(c.p <= bid_p && c.s > 0)
+            {
+                Command c2 = qbuy.top();
+                qbuy.pop();
+                trade_t amount = min(c2.s, c.s);
+                trade(amount, bid_p, c.id);
+                c2.s -= amount;
+                c.s -= amount;
+                buys[bid_p] -= amount;
+                sells[c.p] -= amount;
+                if(c2.s > 0) qbuy.push(c2);
+                update_quote(); 
+            }
+            qsell.push(c);
+        }
+        clearQueue();
+        update_quote();
+        print_quote();
+    }
 }
 
-
-void update_quote()
-{
-	queue<Cmd> t;
-	ask_p = qbuy.top().p;
-	ask_s = bid_s = 0;
-	bid_p = qsell.top().p;
-	while(true)
-	{
-		if(qbuy.empty())
-		{
-			bid_p = 0;
-			bid_s = 0;
-			break;
-		}
-		Cmd c = qbuy.top();
-		if(!c.p == bid_p) break;
-		qbuy.pop();
-		t.push(c);
-	}
-	while(!t.empty())
-	{
-		Cmd c = t.front();
-		t.pop();
-		bid_s += c.s;
-		qbuy.push(c);
-	}
-	while(true)
-	{
-		if(qsell.empty())
-		{
-			ask_p = 99999;
-			ask_s = 0;
-			break;
-		}
-		Cmd c = qsell.top();
-		if(!c.p == ask_p) break;
-		qsell.pop();
-		t.push(c);
-	}
-	while(!t.empty())
-	{
-		Cmd c = t.front();
-		t.pop();
-		ask_s += c.s;
-		qsell.push(c);
-	}
-}
-		   
 int main()
 {
-	trade_t N;
-	while(cin >> N)
-	{
-		clear();
-		for(trade_t i = 1; i <= N; i++)
-		{
-			clear_queue();
-			string c;
-			cin >> c;
-			if(c[0] == 'C')
-			{
-				trade_t id;
-				cin >> id;
-				d[id] = true;
-			}
-			if(c[0] == 'B')
-			{
-				Cmd c;
-				c.id = i;
-				cin >> c.s >> c.p;
-				while(c.p >= ask_p && c.s > 0)
-				{
-				    if(qsell.empty()) break;
-					Cmd c2 = qsell.top();
-					qsell.pop();
-					trade_t amount = min(c2.s, c.s);
-					print_trade(amount,ask_p);
-					c.s -= amount;
-					c2.s -= amount;
-					if(c2.s > 0)qsell.push(c2);
-				}
-				qbuy.push(c);
-			}
-			if(c[0] == 'S')
-			{
-				Cmd c;
-				c.id = i;
-				cin >> c.s >> c.p;
-				while(c.p <= bid_p && c.s > 0)
-				{
-				    if(qbuy.empty()) break;
-					Cmd c2 = qbuy.top();
-					qbuy.pop();
-					trade_t amount = min(c2.s, c.s);
-					print_trade(amount, bid_p);
-					c.s -= amount;
-					c2.s -= amount;
-					if(c2.s > 0) qbuy.push(c2);
-				}
-				qsell.push(c);
-			}
-			clear_queue();
-			update_quote();
-			print_quote();
-		}
-	}
-	return 0;
+    unsigned N;
+    while(cin>>N)
+    {
+    	while(!qbuy.empty()) qbuy.pop();
+    	while(!qsell.empty()) qsell.pop();
+        _main(N);
+    }
 }
